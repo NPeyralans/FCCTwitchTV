73a47867a4742b25f2b3b414564a7ebb2d55531f
NPeyralans <Nikolos.Peyralans@gmail.com>
2016-07-13 12:37:40 +0300

    Add getUserData returning objects needs object array

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,4 +4,23 @@ angular.module('twitchtvApp')
         
         //$scope.title = mainFactory.getTitle();
         
+        $scope.users = mainFactory.getUsers();
+        console.log('Users passed to GetData: ' + $scope.users);
+        
+        $scope.usersData = mainFactory.getData($scope.users);
+        if ($scope.usersData) {
+            console.log($scope.usersData);
+        } 
+        //console.log('usersData in ctrl\n' + $scope.usersData);
+        
+        // See if they are online
+        
+
+        
+        
+        
+        
+        
+        
+        
     }]);
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -5,12 +5,35 @@ angular.module('twitchtvApp')
         var mainFac = {};
         
         /* Test function to verify angular linking
-        mainFac.getTitle = function() {
-            console.log('gettitle');
-            return "THIS IS TEH TITEL";
+            mainFac.getTitle = function() {
+                console.log('gettitle');
+                return "THIS IS TEH TITEL";
         } */
         
+        mainFac.getFCC = function() {                                  $http.get('https://api.twitch.tv/kraken/streams/freecodecamp').then(function(data) {
+                    console.log(data);
+                })
+        }
         
+        mainFac.getUsers = function() {
+            console.log('inside getusers');
+            return ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+        }
+        
+        mainFac.getData = function(users) { 
+            
+            var userData = [];
+            
+            for (var i = 0; i < users.length; i++) {
+                $http.get('https://api.twitch.tv/kraken/streams/' + users[i]).then(function(data) {
+                        //console.log(data);
+                        userData.push(data);
+                    })
+            };
+            
+            return userData;
+            
+        }
         
         return mainFac;
         
